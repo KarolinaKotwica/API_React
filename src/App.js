8bd4883aba2dbbea8fa9729da25cafe6f04a7f0f
Karolina <karolina.kotwica@powercoders.org>
2023-03-29 23:47:37 +0200

    Update App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Search_bar from './components/Search_bar';
 import Image_List from './components/Image_list';
 import Image_show from './components/Image_show';
+import searchImages from './api';
 
 function App() {
+
+  const handleSubmit = (term) => {
+      
+    // searchImages(term)
+  }
+
   return ( <div>
-      <Search_bar />
-      <h1>App</h1>
+      <Search_bar onSubmit={handleSubmit} />
     </div>
   );
 }
--- src/components/Search_bar.js
+++ src/components/Search_bar.js
@@ -1,6 +1,25 @@
-const Search_bar = () => {
+import { useState } from "react";
+
+const Search_bar = ({onSubmit}) => {
+    const [term, setTerm] = useState('')
+
+    const handleChange = (e) => {
+        setTerm(e.target.value)
+    }
+
+    const handleFromSubmit = (e) => {
+        e.preventDefault();
+         
+        onSubmit(term);
+    }
+
     return (
-        <div></div>
+        <div>
+            <form onSubmit={handleFromSubmit}>
+                <input value={term} type='text' onChange={handleChange}></input>
+                <p>{term}</p>
+            </form>
+        </div>
     )
 }
 

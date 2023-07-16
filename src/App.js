923af5c88e237a85e45432824dc1fa63c4dd9725
Karolina <karolina.kotwica@powercoders.org>
2023-07-16 13:57:54 +0200

    Update app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Search_bar from './components/Search_bar';
 import Image_List from './components/Image_list';
 import searchImages from './api';
+import "./App.css"
 
 function App() {
   const [images, setImages] = useState([])
@@ -13,6 +14,7 @@ function App() {
   }
 
   return ( <div>
+      <h1>What kind of images do you want to display?</h1>
       <Search_bar onSubmit={handleSubmit} />
       <Image_List images={images} />
     </div>
